afc5914d00b9337b100dc216b298267ca8dfb408
LAD <luigiandrea@gmail.com>
2019-11-16 23:22:34 -0500

    remove print from GetFuncName

--- test-helper.go
+++ test-helper.go
@@ -9,7 +9,6 @@ import (
 
 //GetFuncName return the name of the function passed as parameter
 func GetFuncName(f interface{}) string {
-	fmt.Println(runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name())
 	link := strings.Split(runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name(), ".")
 	return link[len(link)-1]
 }
